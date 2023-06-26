2c6cff6c417d09e836d5e497ae6f13f6e8ff0e2b
jehmes <jeihmesthales@hotmail.com>
2023-06-25 21:17:20 -0300

    added api-gateway path

--- src/main/java/com/ecommerce/payment/services/PaymentService.java
+++ src/main/java/com/ecommerce/payment/services/PaymentService.java
@@ -1,8 +1,6 @@
 package com.ecommerce.payment.services;
 
 import com.ecommerce.payment.dtos.OrderDto;
-import com.ecommerce.payment.dtos.PaymentDto;
-import com.ecommerce.payment.models.Payment;
 
 public interface PaymentService {
     void saveAndPublish(OrderDto orderDto);
